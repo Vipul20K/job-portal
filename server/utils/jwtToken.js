85900f62100a4cb0910674325d02367f4c9340f7
Vipul Kumar <143587680+Vipul20K@users.noreply.github.com>
2025-07-24 20:48:27 +0530

    Update jwtToken.js

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -4,7 +4,9 @@ export const sendToken = (user, statusCode, res, message) => {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, // Set httpOnly to true
+    httpOnly: true,
+    secure: true,          // 🔥 Required for HTTPS (i.e., Vercel + Render)
+    sameSite: "None",      // 🔥 Required to allow cross-origin cookie sharing
   };
 
   res.status(statusCode).cookie("token", token, options).json({
@@ -14,4 +16,3 @@ export const sendToken = (user, statusCode, res, message) => {
     token,
   });
 };
- 
